78b4edd6cf7af991570a23257fad9953a42272bf
先規　勇樹 <yukisaki80@gmail.com>
2021-03-09 17:56:36 +0900

    comment controllerの修正

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -9,14 +9,5 @@ class CommentsController extends Controller
 {
     public function store(Request $request)
     {
-        $params = $request->validate([
-            'post_id' => 'required|exists:posts,id',
-            'body' => 'required|max:2000',
-        ]);
-
-        $post = Post::findOrFail($params['post_id']);
-        $post->comments()->create($params);
-
-        return redirect()->route('posts.show', ['post' => $post]);
     }
 }
